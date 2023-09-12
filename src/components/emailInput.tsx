55d091ee4cf89d09f4954e26afdc310ad8c00f9c
EduSalomao <eduardosalomao33@gmail.com>
2023-09-12 12:24:07 -0300

    initializing repo

---
+++ src/components/emailInput.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+export function EmailInput() {
+  return (
+    <View>
+      <Text>Email</Text>
+    </View>
+  );
+}
